47f87e86503ed25edd6613c47d79a7acd79934d3
TA3DeangeliGonzalezOlaza <ta3deangeligonzalezolaza@hotmail.com>
2017-07-06 19:31:31 -0300

    base de datos con usuarios encriptados

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-06-2017 a las 06:14:32
+-- Tiempo de generación: 07-07-2017 a las 00:30:55
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
@@ -43,13 +43,61 @@ CREATE TABLE `comenta` (
 --
 
 INSERT INTO `comenta` (`id_comen`, `id_usucom`, `id_pubcom`, `comentario`, `denunciado_com`, `responde_com`, `respondido`) VALUES
-(11, 1, 14, 'Otra prueba', 1, 'prueba responde', 1),
+(11, 1, 14, 'Comentario eliminado por administrador', 0, 'Respuesta Eliminada por administrador', 1),
 (13, 1, 14, 'lindo para hablar por chat', 0, '		si buenisimo verda martin  \r\n				   ', 1),
 (15, 1, 14, 'hola\r\n', 0, '', 0),
-(16, 1, 12, 'y peluda?', 1, 'si re peluda', 1),
-(17, 11, 9, 'cuanto me haces por dos?', 0, '		  te los regalo no me dejan dormir\r\n				   ', 1),
-(18, 2, 9, 'prueba si comentario queda en \"no\"', 0, '', 0),
-(19, 1, 14, 'Un comentario para probar un select', 0, '		  \r\n				   Responder', 1);
+(16, 1, 12, 'y peluda?', 0, 'si re peluda', 1),
+(17, 11, 9, 'cuanto me haces por dos?', 0, '		  \r\n				   400 pesos LINCE', 1),
+(18, 2, 9, 'Comentario eliminado por administrador', 0, 'Respuesta Eliminada por administrador', 1),
+(19, 1, 14, 'Un comentario para probar un select', 0, '		  \r\n				   Responder', 1),
+(20, 12, 17, 'aceptas tarjeta?', 0, '', 0),
+(21, 2, 11, 'hola tengo una pregunta', 0, '', 0),
+(22, 2, 11, 'hola tengo otra pregunta', 0, '', 0),
+(23, 2, 9, 'es no es un perro grande!', 0, '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `log`
+--
+
+CREATE TABLE `log` (
+  `id_log` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `nombre_admin` varchar(20) NOT NULL,
+  `fecha` date NOT NULL,
+  `accion` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `log`
+--
+
+INSERT INTO `log` (`id_log`, `id_admin`, `nombre_admin`, `fecha`, `accion`) VALUES
+(1, 11, 'andy', '2017-06-22', 'Sanciono'),
+(2, 11, 'andy', '2017-06-23', 'Sanciono'),
+(3, 11, 'andy', '2017-06-23', 'PremiumSI'),
+(4, 11, 'andy', '2017-06-23', 'PremiumNO'),
+(5, 11, 'andy', '2017-06-23', 'Levantasancion'),
+(6, 11, 'andy', '2017-06-24', 'Sanciono a 11'),
+(7, 11, 'andy', '2017-06-24', 'Sanciono a 12'),
+(8, 11, 'andy', '2017-06-24', 'Levanto sancion a 11'),
+(9, 11, 'andy', '2017-06-24', 'Sanciono a 11'),
+(10, 11, 'andy', '2017-06-24', 'Levanto sancion a 12'),
+(11, 11, 'andy', '2017-06-24', 'Levanto sancion a 11'),
+(12, 11, 'andy', '2017-06-24', 'Sanciono a 11'),
+(13, 11, 'andy', '2017-06-24', 'Levanto sancion a 11'),
+(14, 0, '', '0000-00-00', ''),
+(15, 11, 'andy', '2017-06-24', 'Finalizo Publicacion NÂ° 15'),
+(16, 11, 'andy', '2017-06-24', 'Quito Denuncia de publicacion NÂ° 12'),
+(17, 11, 'andy', '2017-06-24', 'Verifico Pago de Transaccion NÂ° 2'),
+(18, 11, 'andy', '2017-06-24', 'Quito denuncia de comentario NÂ° 16'),
+(19, 11, 'andy', '2017-06-24', 'Elimino comentario NÂ° 18'),
+(20, 11, 'andy', '2017-06-24', 'Elimino comentario NÂ° 18'),
+(21, 11, 'andy', '2017-06-24', 'Sanciono a 1'),
+(22, 11, 'andy', '2017-06-24', 'Levanto sancion a 1'),
+(23, 11, 'andy', '2017-06-24', 'Sanciono a 1'),
+(24, 11, 'andy', '2017-06-24', 'Levanto sancion a 1');
 
 -- --------------------------------------------------------
 
@@ -75,10 +123,15 @@ CREATE TABLE `permuta` (
 
 INSERT INTO `permuta` (`id_permuta`, `id_pub1`, `id_pub2`, `fechap`, `solicita_permuta`, `permutar`, `permuta_concretada`, `id_ususolper`, `id_usurecper`) VALUES
 (11, 12, 16, '2017-06-08', 1, 0, 0, 1, 2),
-(12, 12, 16, '2017-06-08', 1, 1, 0, 1, 2),
+(12, 12, 16, '2017-06-08', 1, 0, 0, 1, 2),
 (13, 11, 17, '2017-06-08', 1, 1, 1, 2, 1),
 (14, 9, 14, '2017-06-08', 1, 0, 0, 2, 1),
-(15, 9, 14, '2017-06-08', 1, 1, 0, 2, 1);
+(15, 9, 14, '2017-06-08', 1, 1, 0, 2, 1),
+(16, 11, 12, '2017-06-08', 1, 1, 0, 2, 1),
+(17, 9, 21, '2017-06-24', 1, 1, 0, 11, 1),
+(18, 12, 21, '2017-06-24', 1, 1, 0, 11, 2),
+(19, 12, 21, '2017-06-24', 1, 1, 0, 11, 2),
+(20, 12, 21, '2017-06-24', 1, 1, 0, 11, 2);
 
 -- --------------------------------------------------------
 
@@ -90,7 +143,7 @@ CREATE TABLE `persona` (
   `id_per` int(11) NOT NULL,
   `ci_per` int(11) DEFAULT NULL,
   `nick_per` varchar(20) DEFAULT NULL,
-  `password_per` varchar(20) DEFAULT NULL,
+  `password_per` varchar(200) DEFAULT NULL,
   `nom1_per` varchar(20) DEFAULT NULL,
   `nom2_per` varchar(20) NOT NULL,
   `ape1_per` varchar(20) DEFAULT NULL,
@@ -105,9 +158,19 @@ CREATE TABLE `persona` (
 --
 
 INSERT INTO `persona` (`id_per`, `ci_per`, `nick_per`, `password_per`, `nom1_per`, `nom2_per`, `ape1_per`, `ape2_per`, `email_per`, `tel_per`, `dir_per`) VALUES
-(1, 11111111, 'fabri', 'fabo', 'fabri', 'fabri', 'olaza', 'olaza', 'mail@mail.com', 14521, 'mi direccion'),
-(2, 11111111, 'pichu', 'pichu', 'pi', 'ka', 'chu', 'pokemon', 'pikachu@picapica.com', 99090909, 'isla pokemon'),
-(11, 11111111, 'andy', 'andres', 'andy', 'andy', 'ape1', 'ape2', 'mail@mail.com', 999999999, 'la direccion');
+(1, 11111111, 'fabri', '5a249da2a61e7d4d05fdba7fa6a14507', 'fabri', 'fabri', 'olaza', 'benenatti', 'mail@mail.com', 14521, 'mi direccion'),
+(2, 11111111, 'pichu', '12345', 'pi', 'ka', 'chu', 'pokemon', 'pikachu@picapica.com', 99090909, 'isla pokemon'),
+(11, 11111111, 'andy', '231badb19b93e44f47da1bd64a8147f2', 'andy', 'andy', 'ape1', 'ape2', 'mail@mail.com', 999999999, 'la direccion'),
+(12, 11111111, 'Usuario', '12345', 'nombre1', 'nombre2', 'apellido1', 'apellido2', 'Usuario@hotmail.com', 123123123, 'en algun lugar del mundo.'),
+(13, 11111111, 'puto', '827ccb0eea8a706c4c34a16891f84e7b', 'puto', 'puto', 'puto', 'puto', 'puto@chumingalapocha', 234234244, 'puto'),
+(14, 65656565, 'ertreter', '827ccb0eea8a706c4c34a16891f84e7b', 'rertertert', 'tertert', 'erterter', 'ertertert', 'tretretertert', 2147483647, 'etertertretet'),
+(15, 11111111, 'gsgs', '7cdd347489383ef85cb3526af2a7a0aa', 'gsgs', 'gsgs', 'gsgs', 'gsgs', 'gsgs@gsgs', 24237000, 'gsgs'),
+(16, 11111111, 'google', 'c822c1b63853ed273b89687ac505f9fa', 'google', 'google', 'google', 'google', 'google@google.com', 45555555, 'google'),
+(17, 11111111, 'pachuchi', '0adf74d947875858a9ef3b704b8210d5', 'pachuchi', 'pachuchi', 'pachuchi', 'pachuchi', 'pachuchi@pachuchi', 2147483647, 'pachuchi'),
+(18, 22222222, 'LINCE', '2ef0e020fb1a7cd686518922fd03eb9d', 'LINCE', 'LINCE', 'LINCE', 'LINCE', 'LINCE@asdasdasd', 23333333, 'LINCE'),
+(19, 23424324, 'Macho', 'ef59e23314cdfb29c721a1a85f828807', 'Macho', 'Macho', 'Macho', 'Macho', 'macho@msvho.com', 23423424, 'Macho'),
+(20, 38177122, 'donatelo', 'e1f2f91dfb76f57ee637e3ba2a1f3c96', 'donatelo', 'donatelo', 'donatelo', 'donatelo', 'donatelo@donatelo', 24444444, 'donatelo'),
+(21, 45645647, 'cholo', '32eb50d8e68e5ebd93d0b3291de250cb', 'donatelo', 'donatelo', 'donatelo', 'donatelo', 'cholo@donatelo', 21123131, 'donatelo');
 
 -- --------------------------------------------------------
 
@@ -122,9 +185,9 @@ CREATE TABLE `publicacion` (
   `precio_pub` int(11) DEFAULT NULL,
   `stock_pub` int(11) DEFAULT NULL,
   `descripcion_pub` varchar(400) DEFAULT NULL,
-  `img01_pub` varchar(20) DEFAULT NULL,
-  `img02_pub` varchar(20) NOT NULL,
-  `img03_pub` varchar(20) NOT NULL,
+  `img01_pub` varchar(100) DEFAULT NULL,
+  `img02_pub` varchar(100) NOT NULL,
+  `img03_pub` varchar(100) NOT NULL,
   `nuevo_pub` varchar(20) DEFAULT NULL,
   `fecha_pub` date DEFAULT NULL,
   `acepta_permuta_pub` varchar(20) DEFAULT NULL,
@@ -138,14 +201,18 @@ CREATE TABLE `publicacion` (
 --
 
 INSERT INTO `publicacion` (`id_pub`, `id_usup`, `nom_pub`, `precio_pub`, `stock_pub`, `descripcion_pub`, `img01_pub`, `img02_pub`, `img03_pub`, `nuevo_pub`, `fecha_pub`, `acepta_permuta_pub`, `categoria_pub`, `denuncia_pub`, `activa`) VALUES
-(9, 1, 'perro grande', 200, 1, 'descripcion local', '', '', '', 'nuevo', '2017-06-05', 'permuta', 'OTROS', 0, 'si'),
-(10, 1, 'Gato', 300, 2, 'descripcion local', '', '', '', 'usado', '2017-06-05', 'permuta', 'OTROS', 0, 'si'),
-(11, 1, 'dragon grande', 450, 11, 'prueba modificacion', '', '', '', 'nuevo', '2017-06-04', 'permuta', 'OTROS', 0, 'si'),
-(12, 2, 'OSO PELUDO', 10, 10, 'Negro y grande y peludo', '', '', '', 'usado', '2017-06-02', 'permuta', 'OTROS', 1, 'si'),
-(14, 2, 'Prueba articulo', 123, 225, 'Prueba para ver si queda premium', '', '', '', 'nuevo', '2017-05-30', 'nopermuta', 'MODA', 0, 'si'),
-(15, 1, 'rodri', 2, 1, 'ta divino', '', '', '', 'usado', '2017-06-02', 'permuta', 'MODA', 0, 'no'),
-(16, 1, 'prueba activa', 233, 26, 'una prueba si ingresa este articulo', '', '', '', 'nuevo', '2017-06-05', 'nopermuta', 'VEHICULOS', 0, 'si'),
-(17, 2, 'prueba sobre calificacion.', 344, 122, 'una prueba para verificar ciertas cosas', '', '', '', 'usado', '2017-06-06', 'nopermuta', 'MODA', 0, 'si');
+(9, 1, 'perro grande', 200, 1, 'descripcion local', 'perrogrande.png', '', '', 'nuevo', '2017-06-05', 'permuta', 'OTROS', 0, 'si'),
+(10, 1, 'Gato', 300, 1, 'descripcion local', 'gato.jpg', '', '', 'usado', '2017-06-05', 'permuta', 'OTROS', 0, 'si'),
+(11, 1, 'dragon grande', 450, 11, 'prueba modificacion', 'dragon.jpg', '', '', 'nuevo', '2017-06-04', 'permuta', 'OTROS', 0, 'no'),
+(12, 2, 'OSO PELUDO', 10, 8, 'Negro y grande y peludo', 'oso peludo.jpg', 'oso2.jpg', 'oso3.jpg', 'usado', '2017-06-02', 'permuta', 'OTROS', 0, 'si'),
+(14, 2, 'Prueba articulo', 123, 225, 'Prueba para ver si queda premium', '', '', '', 'nuevo', '2017-05-30', 'nopermuta', 'MODA', 1, 'no'),
+(15, 1, 'rodri', 2, 1, 'ta divino', 'rodri.jpg', '', '', 'usado', '2017-06-02', 'permuta', 'MODA', 0, 'no'),
+(16, 1, 'prueba activa', 233, 26, 'una prueba si ingresa este articulo', 'activa.jpg', '', '', 'nuevo', '2017-06-05', 'nopermuta', 'VEHICULOS', 0, 'si'),
+(17, 2, 'prueba sobre calificacion.', 344, 121, 'una prueba para verificar ciertas cosas', '', '', '', 'usado', '2017-06-06', 'nopermuta', 'MODA', 0, 'si'),
+(18, 12, 'PUERTA GRANDE MADERA', 3400, 2, 'PUERTA DE MADERA GRANDE PARA INTERIOR.\r\nMEDIDAS 190 cm x 120.\r\nMadera de pino', '', '', '', 'nuevo', '2017-06-10', 'nopermuta', 'HOGAR', 0, 'si'),
+(19, 2, 'auto', 3000, 2, 'Joya nunca taxi', 'GetPhoto (7).jpg', '', '', 'usado', '2017-06-21', 'permuta', 'VEHICULOS', 0, 'si'),
+(20, 2, 'chorizo', 30, 397, 'Caserito caserito', 'chorizo.png', '', '', 'nuevo', '2017-06-21', 'nopermuta', 'OTROS', 0, 'si'),
+(21, 11, 'Ramo de flores', 2000, 56, 'Flores naturalmente naturales', 'd81b7b2628dfb3d206a54a09a9eed438.jpg', '', '', 'nuevo', '2017-06-21', 'permuta', 'OTROS', 0, 'si');
 
 -- --------------------------------------------------------
 
@@ -172,29 +239,35 @@ CREATE TABLE `transaccion` (
 --
 
 INSERT INTO `transaccion` (`id_trans`, `id_usut`, `id_pubt`, `precio_finalt`, `fechat`, `cantidad`, `calificaciont`, `pago_comision`, `comision_monto`, `comentariot`, `califico`) VALUES
-(1, 2, 11, 500, '0000-00-00', 1, -1, 0, 0, 'una porqueria ', 1),
-(2, 2, 11, 500, '0000-00-00', 1, 1, 0, 0, 'muy bueno', 1),
-(3, 2, 11, 500, '0000-00-00', 1, 0, 0, 0, '', 0),
-(6, 2, 10, 500, '0000-00-00', 1, 1, 0, 0, 'excelente', 1),
-(7, 1, 12, 10, '0000-00-00', 5, -1, 0, 0, 'una cagada era blanco, chico y pelado', 1),
-(9, 1, 14, 615, '2017-05-31', 5, 1, 0, 31, 'muy bueno', 1),
+(1, 2, 11, 500, '2017-05-29', 1, -1, 1, 0, 'una porqueria ', 1),
+(2, 2, 11, 500, '2017-06-02', 1, 1, 1, 0, 'muy bueno', 1),
+(3, 2, 11, 500, '2017-06-03', 1, 0, 0, 0, '', 0),
+(6, 2, 10, 500, '2017-06-09', 1, 1, 0, 0, 'excelente', 1),
+(7, 1, 12, 10, '2017-06-07', 5, -1, 0, 0, 'una cagada era blanco, chico y pelado', 1),
+(9, 1, 14, 615, '2017-05-31', 5, 1, 1, 31, 'muy bueno', 1),
 (10, 1, 12, 50, '2017-05-31', 5, 1, 0, 3, 'excelente', 1),
 (12, 1, 12, 10, '2017-06-01', 1, 1, 0, 1, 'muy loco', 1),
 (13, 1, 14, 123, '2017-06-01', 1, 1, 0, 6, 'excelente', 1),
-(14, 1, 14, 123, '2017-06-01', 1, 1, 0, 6, 'buenisimo', 1),
+(14, 1, 14, 123, '2017-06-01', 1, 1, 0, 6, 'Excelente super peludo el oso', 1),
 (15, 1, 14, 123, '2017-06-01', 1, 1, 0, 6, 'muy bueno', 1),
 (16, 1, 14, 123, '2017-06-01', 1, 1, 0, 6, 'muy bueno', 1),
-(17, 1, 14, 123, '2017-06-01', 1, 0, 0, 6, '', 0),
+(17, 1, 14, 123, '2017-06-01', 1, -1, 0, 6, 'recien comentario', 1),
 (20, 1, 14, 369, '2017-06-01', 3, 0, 0, 18, '', 0),
 (21, 1, 12, 10, '2017-06-01', 1, 0, 0, 1, '', 0),
 (22, 1, 12, 50, '2017-06-01', 5, 0, 0, 3, '', 0),
 (23, 1, 14, 369, '2017-06-02', 3, 0, 0, 18, '', 0),
 (27, 11, 9, 200, '2017-06-02', 1, 0, 0, 10, '', 0),
 (28, 11, 9, 200, '2017-06-02', 1, 0, 0, 10, '', 0),
-(29, 11, 10, 600, '2017-06-02', 2, 0, 0, 30, '', 0),
+(29, 11, 10, 600, '2017-06-02', 2, 0, 1, 30, '', 0),
 (30, 1, 12, 10, '2017-06-06', 1, 0, 0, 1, '', 0),
 (31, 2, 10, 300, '2017-06-07', 1, 1, 0, 15, 'muy bueno', 1),
-(32, 1, 12, 10, '2017-06-07', 1, 0, 0, 1, '', 0);
+(32, 1, 12, 10, '2017-06-07', 1, 0, 0, 1, '', 0),
+(33, 2, 10, 300, '2017-06-09', 1, 0, 0, 15, '', 0),
+(34, 12, 17, 344, '2017-06-10', 1, 0, 0, 17, '', 0),
+(35, 13, 12, 10, '2017-07-05', 1, 0, 0, 1, '', 0),
+(36, 11, 20, 30, '2017-07-05', 1, 0, 0, 2, '', 0),
+(37, 11, 20, 30, '2017-07-05', 1, 0, 0, 2, '', 0),
+(38, 11, 20, 30, '2017-07-05', 1, 0, 0, 2, '', 0);
 
 -- --------------------------------------------------------
 
@@ -216,9 +289,19 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usu`, `reputacion_usu`, `suspendido`, `rol`, `premium`, `id_personau`) VALUES
-(1, 0, 0, 'admin', 0, 1),
-(2, 0, 0, 'admin', 1, 2),
-(11, 0, 0, 'user', 0, 11);
+(1, 0, 0, 'user', 0, 1),
+(2, 0, 0, 'user', 1, 2),
+(11, 0, 0, 'admin', 0, 11),
+(12, 0, 0, 'user', 0, 12),
+(13, 0, 0, 'user', 0, 13),
+(14, 0, 0, 'user', 0, 14),
+(15, 0, 0, 'user', 0, 15),
+(16, 0, 0, 'user', 0, 16),
+(17, 0, 0, 'user', 0, 17),
+(18, 0, 0, 'user', 0, 18),
+(19, 0, 0, 'user', 0, 19),
+(20, 0, 0, 'user', 0, 20),
+(21, 0, 0, 'user', 0, 21);
 
 --
 -- Índices para tablas volcadas
@@ -231,6 +314,12 @@ ALTER TABLE `comenta`
   ADD PRIMARY KEY (`id_comen`,`id_usucom`,`id_pubcom`),
   ADD KEY `id_usucom` (`id_usucom`),
   ADD KEY `id_pubcom` (`id_pubcom`);
+
+--
+-- Indices de la tabla `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id_log`);
 
 --
 -- Indices de la tabla `permuta`
@@ -276,32 +365,37 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `comenta`
 --
 ALTER TABLE `comenta`
-  MODIFY `id_comen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_comen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+--
+-- AUTO_INCREMENT de la tabla `log`
+--
+ALTER TABLE `log`
+  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT de la tabla `permuta`
 --
 ALTER TABLE `permuta`
-  MODIFY `id_permuta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_permuta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `id_per` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_per` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT de la tabla `publicacion`
 --
 ALTER TABLE `publicacion`
-  MODIFY `id_pub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_pub` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT de la tabla `transaccion`
 --
 ALTER TABLE `transaccion`
-  MODIFY `id_trans` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_trans` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usu` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id usuario', AUTO_INCREMENT=12;
+  MODIFY `id_usu` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id usuario', AUTO_INCREMENT=22;
 --
 -- Restricciones para tablas volcadas
 --
@@ -494,7 +588,7 @@ CREATE TABLE `pma__recent` (
 --
 
 INSERT INTO `pma__recent` (`username`, `tables`) VALUES
-('root', '[{\"db\":\"e-comerce\",\"table\":\"permuta\"},{\"db\":\"e-comerce\",\"table\":\"persona\"},{\"db\":\"e-comerce\",\"table\":\"usuario\"},{\"db\":\"e-comerce\",\"table\":\"publicacion\"},{\"db\":\"e-comerce\",\"table\":\"transaccion\"},{\"db\":\"e-comerce\",\"table\":\"comenta\"},{\"db\":\"e-commercer\",\"table\":\"persona\"},{\"db\":\"e-commercer\",\"table\":\"permuta\"},{\"db\":\"e-commercer\",\"table\":\"comenta\"},{\"db\":\"e-commercer\",\"table\":\"PERMUTA\"}]');
+('root', '[{\"db\":\"e-comerce\",\"table\":\"persona\"},{\"db\":\"e-comerce\",\"table\":\"usuario\"},{\"db\":\"e-comerce\",\"table\":\"transaccion\"},{\"db\":\"e-comerce\",\"table\":\"publicacion\"},{\"db\":\"e-comerce\",\"table\":\"permuta\"},{\"db\":\"e-comerce\",\"table\":\"log\"},{\"db\":\"e-comerce\",\"table\":\"comenta\"}]');
 
 -- --------------------------------------------------------
 
